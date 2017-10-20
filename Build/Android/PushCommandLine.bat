0783324c22516d9642dbc792a7cda27003aead71
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2017-10-20 21:58:04 +0200

    UE4CommandLine.txt with the -NoSound, -NoTextureStreaming and -NoVerifyGC recommended command line arguments for profiling
    Copied from C:\Program Files\Epic Games\UE_4.17\Engine\Build\Android\UE4Game\
    
    https://wiki.unrealengine.com/Command-Line_Arguments_on_Mobile#Command-line_arguments_on_Android
    
    https://docs.unrealengine.com/latest/INT/Engine/Performance/Options/index.html

---
+++ Build/Android/PushCommandLine.bat
@@ -0,0 +1,2 @@
+@REM copied from C:\Program Files\Epic Games\UE_4.17\Engine\Build\Android\UE4Game\
+%ANDROID_HOME%\platform-tools\adb.exe push UE4CommandLine.txt /mnt/sdcard/UE4Game/UE4GearVR/UE4CommandLine.txt
